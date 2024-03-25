4e864c7f6241bb06a75f69c6c59af88fb28f2ec4
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-03-25 14:06:24 +0800

    feat: config vue router

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const NowPlayingView = {
+  path: "/nowplaying/:tab",
+  name: "NowPlaying",
+  component: NowPlaying,
+};
+
+const routes = [];
+
+// const Routes = {
+//   folder: folder.name,
+//   playlists: playlists.name,
+//   playlist: playlistView.name,
+//   album: albumView.name,
+//   artist: artistView.name,
+//   artistDiscography: artistDiscography.name,
+//   settings: settings.name,
+//   search: search.name,
+//   notFound: notFound.name,
+//   artistTracks: ArtistTracks.name,
+//   favorites: favorites.name,
+//   favoriteAlbums: favoriteAlbums.name,
+//   favoriteTracks: favoriteTracks.name,
+//   favoriteArtists: favoriteArtists.name,
+//   nowPlaying: NowPlayingView.name,
+//   Home: Home.name,
+//   AlbumList: AlbumListView.name,
+//   ArtistList: ArtistListView.name,
+//   Lyrics: LyricsView.name,
+// };
+
+const router = createRouter({
+  mode: "hash",
+  history: createWebHashHistory(),
+  routes,
+});
+
+export { router, Routes };
