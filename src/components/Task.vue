64eadd36fb4b4df41b987653936580b044c0097a
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-03-25 16:08:24 +0800

    fix: fix some styling

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { CheckCircleIcon } from "@heroicons/vue/24/solid";
+
 defineProps({
   task: String,
 });
@@ -6,14 +8,18 @@ defineProps({
 
 <template>
   <div
-    className="flex flex-col text-slate-800 bg-slate-300 my-2 px-3 py-3 w-full rounded-md"
+    class="flex text-slate-800 bg-slate-300 items-center my-2 px-3 py-3 w-full hover:bg-slate-400 hover:scale-[1.03] hover:shadow-slate-400 transition-all rounded-md"
   >
-    <p className="ml-2 font-semibold text-[1.1rem]">{{ task }}</p>
-    <p className="ml-2 text-[0.75rem] text-green-800 font-thin">due Monday</p>
+    <button
+      type="checkbox"
+      className="h-9 rounded-full border-1 border-green-900 focus:ring-1 focus:ring-gray-600"
+    >
+      <check-circle-icon class="w-8" />
+    </button>
+
+    <div className="flex flex-col">
+      <p className="ml-2 font-semibold text-[1.1rem]">{{ task }}</p>
+      <p className="ml-2 text-[0.75rem] text-green-800 font-thin">due Monday</p>
+    </div>
   </div>
-  <button
-    className="bg-red-300  hover:bg-red-500 hover:scale-[1.01] hover:shadow-slate-400 transition-all rounded-md"
-  >
-    Back
-  </button>
 </template>
